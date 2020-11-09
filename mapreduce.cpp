f804773b87e46ef5e73527f06838447eee1cbedc
Jon Takagi <jontakagi@gyoza.reed.edu>
2020-11-08 22:46:21 -0800

    removed incorrect comments

--- mapreduce.cpp
+++ mapreduce.cpp
@@ -15,14 +15,6 @@ using pdata_t = std::vector<pdata_partition_t>;
 
 pdata_t* processed_data;
 MapReduce::partitioner_t partitioner;
-// map matches keys (as strings) to a vector of values and a mutex for the vector
-// map has a mutex for adding keys to the map
-
-// should be a vector of <mutex, map> pairs, where the maps send strings to <mutex, vector<string>> pairs
-// each partition number gets a mutex and a bunch of key-value pairs
-// hashpartition sends keys to numbers in the num_partitions
-// each reducer gets a single key
-
 /*
   map_worker function:
     inputs: map (the function we were passed), the todo vector, and the todo todo_mutex
@@ -52,11 +44,10 @@ void map_worker(MapReduce::mapper_t map, std::vector<char*>* todo, std::mutex* t
 }
 
 const std::string get_next(const std::string& key, int partition_number) {
-    pdata_partition_t* partition = &(processed_data->at(partition_number)); // no bounds checking
+    pdata_partition_t* partition = &(processed_data->at(partition_number)); //we dont check the bound here, but vector.at does. The error message at gives isnt very helpful.
     pdata_map_t* map = &(partition->first);
-    std::vector<std::string>* values_of_key = &(map->at(key).first); // this might just be a copy
+    std::vector<std::string>* values_of_key = &(map->at(key).first); // this is done by reference to ensure no copy is created
     if (values_of_key->size() > 0) {
-        // // std::cout << "size of " << key << " is: " << values_of_key->size() << std::endl;
         std::string rv = values_of_key->back();
         values_of_key->pop_back();
         return rv;
@@ -68,22 +59,17 @@ const std::string get_next(const std::string& key, int partition_number) {
 void reduce_worker(MapReduce::reducer_t reduce, int partition_number) {
     // loop through the map at that partition
     // call reduce on each of the keys in order
-    pdata_partition_t *partition = &(processed_data->at(partition_number));
+    pdata_partition_t *partition = &(processed_data->at(partition_number)); //this is a fun trick we used a lot to make the types line up
     pdata_map_t kv_map = partition->first;
-    // // std::cout << "beginning reduce loop in partition " << partition_number << std::endl;
     for(std::map<std::string,value_set_t>::iterator iter = kv_map.begin(); iter != kv_map.end(); ++iter)
     {
-        // // std::cout << "working..." << std::endl;
-        std::string k =  iter->first;
+        std::string k = iter->first;
         reduce(k, get_next, partition_number);
-        // // std::cout << "done" << std::endl;
-        // ignore value
-        // Value v = iter->second;
     }
 }
 
 namespace MapReduce {
-    void MR_Emit(const std::string& key, const std::string& value) { // called by Map - this adds a key, value pair to some centralized
+    void MR_Emit(const std::string& key, const std::string& value) { // called by Map - this adds a key, value pair to the
         // FIRST: figure out which partition this key belongs in, and get pointers
         //  to the appropriate map
 
@@ -92,8 +78,8 @@ namespace MapReduce {
         pdata_map_t* partition_map = &(partition->first);
         std::mutex* partition_mutex = partition->second.get();
 
-        // note: we're searching the map for the key without locking, which might be a bad idea.
-        // TODO: not this.
+        // check the map to see if it has an entry for the key
+        // if not, lock the map (to prevent double entries) and add the key
         pdata_map_t::iterator key_it = partition_map->find(key);
         if (key_it == partition_map->end()) {
 
@@ -111,18 +97,17 @@ namespace MapReduce {
             partition_mutex->unlock();
         }
 
-        // we may need to find a signaling device that will allow us to prevent
-        //  getting the value_sets out of the map while adding keys to it.
+        // once you get here, the map definitely has an entry for the key
+        // now put the value on the end of the list
 
         value_set_t* value_set = &(partition_map->at(key));
-
-
         std::vector<std::string>* values = &(value_set->first);
         std::mutex* value_set_mutex = value_set->second.get();
         value_set_mutex->lock();
         values->push_back(value);
         value_set_mutex->unlock();
     }
+    // we didnt mess with the logic, but we made it do the typing thing properly
     unsigned long MR_DefaultHashPartition(const std::string& key, int num_partitions) {
         unsigned long hash = 5381;
         int s = (int) key.size();
@@ -143,22 +128,24 @@ namespace MapReduce {
 
         // std::cout << "Setting up processed_data\n";
 
-        for (int i = 0; i < num_reducers; i++) { // hopefully this works to set things up
+        // this creates the elements of the global data structure properly
+        // some fun tricks to make sure the mutex exist because vector elements must be shareable and mutexes arent.
+        for (int i = 0; i < num_reducers; i++) {
           processed_data->emplace_back();
           processed_data->back().second.reset(new std::mutex);
         }
 
         // std::cout << "Setting up task vector\n";
-        // set up task vector;
+        // set up task vector so that workers can find stuff to do (and lock while they get it)
         std::vector<char*> todo;
         std::mutex todo_mutex;
         for (int i = 1; i < argc; i++) {
-          todo.push_back(argv[i]);
+            todo.push_back(argv[i]);
         }
 
 
         // std::cout << "Setting up mapper_threads\n";
-        // set up mapper thread vector;
+        // create num_mappers map_workers, start them going
         std::vector<std::thread> mapper_threads;
         for(int i = 0; i < num_mappers; i++) {
           mapper_threads.push_back(std::thread(map_worker, map, &todo, &todo_mutex));
@@ -172,17 +159,19 @@ namespace MapReduce {
         }
 
         // std::cout << "Setting up num_reducers\n";
+        // create num_reducers reducer workers, start them going
         std::vector<std::thread> reducer_threads;
         for (int i = 0; i < num_reducers; i++) {
           reducer_threads.push_back(std::thread(reduce_worker, reduce, i));
         }
 
         // std::cout << "Waiting for reducer_threads to finish\n";
+        // wait for them to finish
         while (reducer_threads.size() > 0) {
           reducer_threads.back().join();
           reducer_threads.pop_back();
         }
         // std::cout << "Done!\n";
-        delete processed_data; // do we also need to free all the strings in the map?
+        delete processed_data; // all the data is here, so it all frees nicely. this makes it all worth it.
     }
 }
